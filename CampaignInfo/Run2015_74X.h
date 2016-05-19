92d7dd604acfb701c190e476c91cb762b7959aca
Luca Scodellaro <luca.scodellaro@cern.ch>
2016-05-19 14:42:07 +0200

    Downdating wp for 74X

--- CampaignInfo/Run2015_74X.h
+++ CampaignInfo/Run2015_74X.h
@@ -11,7 +11,7 @@ const int nSupportedTaggers = 2;
 TString SupportedTaggerName[nSupportedTaggers] = {"JP", "CSVv2"};
 float MinJetPt[3][nSupportedTaggers] = { { 30.,  30.},   {30., 30.}, {  20.,   20.} };
 float MaxJetPt[3][nSupportedTaggers] = { {670., 670.}, {670., 670.}, {1000., 1000.} };
-float TaggerWP[nSupportedTaggers][3] = { {0.245, 0.515, 0.760}, {0.460, 0.800, 0.935} };
+float TaggerWP[nSupportedTaggers][3] = { {0.275, 0.545, 0.790}, {0.605, 0.890, 0.970} };
 
 const int nFastSimDatasets = 4;
 TString FastSimDatasetName[nFastSimDatasets] = {"_T1tttt_1200_800", "_T1tttt_1500_100", "_T1bbbb_1000_900", "_T1bbbb_1500_100"};
